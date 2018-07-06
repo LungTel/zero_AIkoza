bbb37d900e61754ff812491830cf433119f5f7ff
github user LungTel <taiki50891@yahoo.co.jp>
2018-07-06 21:01:39 +0900

    20180706_新たにファイルの追加

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+a = {"Taro":1995, "Hanako":1986}
+
+print a
+
+b = a["Taro"]
+print b
+
+a["Jiro"] = 1998
+print a
+
+a["Hanako"] = 1990
+print a
+
+
+c = {1:1991, 2:1992}
+print c
+
+d = c[2]
+print d
+
+
---
+++ tuple.py
@@ -0,0 +1,26 @@
+a = [2012, 2013, 2014]
+
+b = (2012, 2013, 2014)
+
+print a
+print b
+
+print a[1]
+print b[2]
+
+a[1] = 2016
+print a[1]
+
+
+#b[1] = 2016
+#print b
+
+
+a.append(2015)
+print a
+
+# b.append(2015)
+# print b
+
+
+
